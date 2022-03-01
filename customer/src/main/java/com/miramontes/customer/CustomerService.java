2e367a7781439c9afc4ae2dd605861ad7a0e9ba8
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-03-01 15:42:57 -0600

    registerCustomer() in CustomerService

--- customer/src/main/java/com/miramontes/customer/CustomerService.java
+++ customer/src/main/java/com/miramontes/customer/CustomerService.java
@@ -1,6 +1,17 @@
 package com.miramontes.customer;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public record CustomerService() {
     public void registerCustomer(CustomerRegistrationRequest request) {
+        CustomerModel customer = CustomerModel.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email()).build();
+
+        // todo: validate if email is valid
+        // todo: validate email is not taken
+        // todo: store customer in db
     }
 }
