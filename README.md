7881227c5a6fd621aec269829280c2cd20c173e8
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-03-02 16:04:33 -0600

    Established JPA details to Customer Model

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ App following tuto https://www.youtube.com/watch?v=1aWhYEynZQw
 3) Docker
 4) Postgres
 5) PGAdmin
+6) @SequenceGenerator
+7) @Builder
 
 # Topics
 1) @RestController
--- customer/src/main/java/com/miramontes/customer/CustomerModel.java
+++ customer/src/main/java/com/miramontes/customer/CustomerModel.java
@@ -1,11 +1,23 @@
 package com.miramontes.customer;
 
 import lombok.*;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerModel {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String firstName;
     private String lastName;
